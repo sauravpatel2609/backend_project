f8f53315d6c84f9a5dad246b2615daeb3c3b0f8b
Saurav <sauravpatel90768@gmail.com>
2025-03-25 23:57:13 +0530

    database connected

---
+++ src/index.js
@@ -0,0 +1,45 @@
+//require("dotenv").config();
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+});
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+
+
+// const app=express()
+
+
+// ((async)=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`, )
+//         console.log("Database connected successfully")
+//         app.on("error",()=>{
+//             console.log("Error ",error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env
+//         })
+        
+//     } catch (error) {
+//         console.error(error)
+//         throw error
+//     }
+// })()
