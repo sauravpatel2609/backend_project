ec0cf66d9b0cb0dca50a3c81fb7985809ad74197
Saurav <sauravpatel90768@gmail.com>
2025-03-27 01:05:01 +0530

    ADD USER AND VIDEO MODEL

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    console.log("Database connected successfully")
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 
 
